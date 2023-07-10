fabee44efde055884b9a65507123b4185c009d79
@Cheptoo <97380715+hellen2021@users.noreply.github.com>
2023-07-10 15:17:10 +0300

    Update index3.py

--- dataEng/yahoo/index3.py
+++ dataEng/yahoo/index3.py
@@ -14,17 +14,8 @@ print(response.text)
 
 # table = soup.find("table", class_="W(100%) M(0)")
 # print(table)
-# if table is not None:
-#     data = []
-#     rows = table.find_all("tr")
 
-#     for row in rows:
-#         cols = row.find_all("td")
-#         if len(cols) == 7:  # Check if it's a valid row
-#             row_data = [col.text.strip() for col in cols]
-#             data.append(row_data)
+# data = []
+# rows = table.find_all("tr")
 
-#     df = pd.DataFrame(data, columns=["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"])
-#     print(df)
-# else:
-#     print("Table not found.")
+
