5ac3071ef3e3dd1074b10dea2c9daf8c271c4d8e
hellen2021 <elleville23@gmail.com>
2023-07-12 18:22:41 +0300

    using yfinance

--- yahoo/stocks.py
+++ yahoo/stocks.py
@@ -2,6 +2,7 @@ import yfinance as yf
 
 # Create a ticker object for the stock you want to retrieve data for
 ticker = yf.Ticker("AAPL")
+# msft_ticker = yf.Ticker("msft")
 
 # Get historical price data
 historical_data = ticker.history(period="1mo")
